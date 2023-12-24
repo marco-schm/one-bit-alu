0aaf7ffe742cbc0ed02d83b26edd209075ec746e
Marco <marco.schmitt.mail@gmail.com>
2023-12-24 10:06:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# 1-BIT ALU Simulator
+![ALU](https://upload.wikimedia.org/wikipedia/commons/1/17/Simplified-ALU.svg)
+
+
+This project is a simple ALU (Arithmetic Logic Unit) simulator implemented in C. The purpose of this project is to provide a hands-on learning experience for the "Grundlagen Informatik" (Fundamentals of Computer Science) module. The simulator allows users to input boolean values for 'a' and 'b' and select an ALU operation from a predefined set.
+
+## Usage
+
+To run the simulator, use the following command:
+
+```
+./alu_simulator <value_for_a> <value_for_b> <alu_operation>
+```
+
+- <value_for_a> and <value_for_b> should be either '0' or '1'.
+- <alu_operation> is a number between 0 and 7, representing different ALU operations.
+
+### Possible ALU Operations:
+- 0: DEACTIVATE
+- 1: A
+- 2: NOT A
+- 3: A AND B
+- 4: A OR B
+- 5: A XOR B
+- 6: A + B
+- 7: B - A
+
+### Example 
+```
+./one_bit_alu 1 0 3
+```
+This example sets 'a' to true, 'b' to false, and performs the ALU operation 'A AND B'. The result will be displayed as 'R: &lt;result&gt;, D: <carry_out>'.
+
+## Building the project
+To build the project, follow these steps:
+
+1) Clone the repository to your local machine:
+```
+git clone https://github.com/your-username/one-bit-alu.git
+```
+
+2) Navigate to the project directory:
+```
+cd one-bit-alu
+```
+
+3) Compile the source code using a C compiler. For example, using gcc:
+```
+gcc -o one_bit_alu main.c
+```
+
+4) Run the compiled executable:
+```
+./one_bit_alu <value_for_a> <value_for_b> <alu_operation>
+```
+
+## Purpose
+This project is intended for educational purposes, specifically for the "Grundlagen Informatik" module. 
+It provides a practical illustration of basic digital logic concepts and ALU operations.
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
---
+++ main.c
@@ -0,0 +1,158 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct {
+    bool sum;
+    bool carry_out;
+} AdderResult;
+
+bool and_gate(bool input1, bool input2){
+    return input1 && input2;
+}
+
+bool or_gate(bool input1, bool input2){
+    return input1 || input2;
+}
+
+bool xor_gate(bool input1, bool input2){
+    return input1 ^ input2;
+}
+
+bool inverter(bool input1, bool activate) {
+    return xor_gate(input1,activate);
+}
+
+bool gate(bool input1, bool activate){
+    return and_gate(input1, activate);
+}
+
+AdderResult adder(bool input1, bool input2, bool carry_in) {
+    AdderResult result;
+    result.sum = input1 ^ input2 ^ carry_in;
+    result.carry_out = (input1 && input2) | (carry_in && (input1 ^ input2));
+    return result;
+}
+
+bool multiplexer(bool input1, bool input2, bool input3, bool input4, bool select1, bool select2){
+    return (input1 && !select1 && !select2) || (input2 && select1 && !select2) ||
+            (input3 && !select1 && select2) || (input4 && select1 && select2);
+
+}
+
+
+void print_alu_operations() {
+    printf("Possible ALU Operations:\n");
+    printf("  0: DEACTIVATE\n");
+    printf("  1: A\n");
+    printf("  2: NOT A\n");
+    printf("  3: A AND B\n");
+    printf("  4: A OR B\n");
+    printf("  5: A XOR B\n");
+    printf("  6: A + B\n");
+    printf("  7: B - A\n");
+}
+
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 4) {
+        printf("Usage: %s <value_for_a> <value_for_b> <alu_operation>\n", argv[0]);
+        printf("  where:\n");
+        printf("    <value_for_a> and <value_for_b> are either '0' or '1'\n");
+        printf("    <alu_operation> is a number between 0 and 7\n");
+        print_alu_operations();
+        return 1;
+    }
+
+    bool a = (argv[1][0] == '1') ? true : false;
+    bool b = (argv[2][0] == '1') ? true : false;
+
+    bool f3;
+    bool f2;
+    bool f1;
+    bool f0;
+
+    switch (argv[3][0]) {
+        case '0':
+            //DEACTIVATE
+            f3 = false;
+            f2 = false;
+            f1 = false;
+            f0 = false;
+            break;
+
+        case '1':
+            //A
+            f3 = false;
+            f2 = false;
+            f1 = false;
+            f0 = true;
+            break;
+
+        case '2':
+            //NOT A
+            f3 = true;
+            f2 = false;
+            f1 = false;
+            f0 = true;
+            break;
+
+        case '3':
+            //A_AND_B
+            f3 = false;
+            f2 = true;
+            f1 = false;
+            f0 = false;
+            break;
+
+        case '4':
+            //A_OR_B;
+            f3 = false;
+            f2 = true;
+            f1 = false;
+            f0 = true;
+            break;
+
+        case '5':
+            //A_XOR_B;
+            f3 = false;
+            f2 = true;
+            f1 = true;
+            f0 = false;
+            break;
+
+        case '6':
+            //A_PLUS_B;
+            f3 = false;
+            f2 = true;
+            f1 = true;
+            f0 = true;
+            break;
+
+        case '7':
+            //B_MINUS_A;
+            f3 = true;
+            f2 = true;
+            f1 = true;
+            f0 = true;
+            break;
+
+        default:
+            printf("Invalid ALU operation: %s\n", argv[3]);
+            print_alu_operations();
+            return 1; // Return value 1 indicate
+    }
+
+    bool a1 = inverter(a, f3);
+    bool b1 = gate(b, f2);
+    bool and_result = and_gate(a1, b1);
+    bool or_result = or_gate(a1, b1);
+    bool xor_result = xor_gate(a1, b1);
+    AdderResult adder_result = adder(a1, b1, f3);
+    bool multiplexer_result = multiplexer(and_result, or_result, xor_result,
+                                                  adder_result.sum, f0, f1);
+
+    printf("R: %d, D: %d", multiplexer_result, adder_result.carry_out);
+    return 0;
+
+}
